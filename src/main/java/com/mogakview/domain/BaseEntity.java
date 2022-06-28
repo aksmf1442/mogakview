a473ca441f5002358418bf21c598e0d6f4077ece
aksmf1442 <aksmf1442@naver.com>
2022-06-28 21:37:31 +0900

    Fix: db에 createdAt, updatedAt 필드 안 들어가는 현상 들어가도록 수정

--- src/main/java/com/mogakview/domain/BaseEntity.java
+++ src/main/java/com/mogakview/domain/BaseEntity.java
@@ -1,7 +1,7 @@
 package com.mogakview.domain;
 
 import java.time.LocalDateTime;
-import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,17 +9,18 @@ import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @CreatedDate
-//    @Column
     private LocalDateTime createdAt;
 
     @LastModifiedDate
